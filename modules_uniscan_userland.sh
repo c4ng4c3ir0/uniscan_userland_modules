383792cf55d994b333d16929e9cd5c8490da8977
juaumvictor <54048948+juaumvictor@users.noreply.github.com>
2020-09-09 18:58:51 -0300

    Add files via upload
    This bash file corrects the flaws when trying to run UNISCAN on UserLAnd. Enjoy it.

---
+++ modules_uniscan_userland.sh
@@ -0,0 +1,21 @@
+#by @juauumv
+
+cpan -i Moose
+
+cpan -i threads
+
+cpan -i threads::shared
+
+cpan -i Thread::Queue
+
+cpan -i HTTP::Response
+
+cpan -i HTTP::Request
+
+cpan -i LWP::UserAgent
+
+cpan -i Net::SSLeay
+
+cpan -i Getopt::Std
+
+cpan -i LWP::HTTPS
